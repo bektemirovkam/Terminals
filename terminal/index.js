aafac74573f9ae85602884dc6e977aa8d3f7f600
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2021-03-28 00:05:24 +0600

    change Id

--- terminal/index.js
+++ terminal/index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io-client");
 
-const TERMINAL_ID = "605c35fc6db9dc1778aa3571";
+const TERMINAL_ID = "605f726772572d00156d01ff";
 
 // const socket = io("http://localhost:8888");
 const socket = io("https://immense-spire-12966.herokuapp.com/");
